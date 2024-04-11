9e60b2ff62699fd83edc515c97e0cf762ae734bf
Rick <ricardo.henriques@accedo.tv>
2024-04-11 15:11:22 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
+        </a>
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
+        </a>
+      </div>
+      <h1>LnL Video App</h1>
+      <div className='card'>
+        <video width={1920} height={1080} controls>
+          <source
+            type='video/mp4'
+            src='http://pmd.cdn.turner.com/cnn/big/cnn10/2020/11/09/ten-1111.cnn_3470661_ios_1240.mp4'
+          />
+        </video>
+      </div>
+      <p className='read-the-docs'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
